1ae3ef49a2078a3eeb698b2126ea1d6c5dc59b8d
First <suphanan@softnix.co.th>
2021-04-26 18:56:17 +0700

    custom theme and home page

--- src/home/homePage.tsx
+++ src/home/homePage.tsx
@@ -1,29 +1,48 @@
-
-const HomePage = () => {
+import { url } from 'node:inspector'
+import * as React from 'react'
+import myPic from '../static/images/000009.jpeg'
+const HomePage: React.FC<any> = () => {
   return (
     <>
-      <div className="py-12 bg-white">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="lg:text-center">
-            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-              H!, everyone.
-            </h2>
-            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              A better way to let you know me
-            </p>
-            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-              Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum
-              cupiditate veritatis in accusamus quisquam.
-            </p>
+      {/* <img src={myPic} alt="my_pic" className="object-cover w-full ..." /> */}
+      <div className="h-screen">
+        <div className="relative h-full overflow-hidden bg-yellow-400">
+          <div className="absolute z-30 flex w-full h-full">
+            <div className="relative flex items-stretch z-30 w-5/6 px-6 py-8 text-white md:py-10 md:w-1/2">
+              <div className="self-center mx-4">
+                <h1 className="text-6xl font-black">Welcome to my playgroud</h1>
+                <p className="my-8 leading-relaxed">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat eveniet earum
+                  voluptatibus tempora veniam?{' '}
+                </p>
+              </div>
+            </div>
+            <div className="absolute top-0 right-0 flex w-full h-full">
+              <div className="w-1/6 h-full bg-yellow-400"></div>
+              <div className="relative w-1/6">
+                <svg
+                  fill="currentColor"
+                  viewBox="0 0 100 100"
+                  className="absolute inset-y-0 z-20 h-full text-yellow-400"
+                >
+                  <polygon id="diagonal" points="0,0 100,0 50,100 0,100"></polygon>
+                </svg>
+                <svg
+                  fill="currentColor"
+                  viewBox="0 0 100 100"
+                  className="absolute inset-y-0 z-10 h-full ml-6 text-white opacity-50"
+                >
+                  <polygon points="0,0 100,0 50,100 0,100"></polygon>
+                </svg>
+              </div>
+            </div>
           </div>
-
-          <div className="mt-10">
-            <h1>Home Page</h1>
+          <div className="absolute top-0 right-0 block w-9/12 h-full">
+            <img src={myPic} alt="my_pic" className="object-cover min-w-full h-full" />
           </div>
         </div>
       </div>
     </>
   )
 }
-
 export default HomePage
--- src/layout/NevMenu.tsx
+++ src/layout/NevMenu.tsx
@@ -2,68 +2,65 @@ import * as React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 
+import myIcon from '../static/icons/sunflower.svg'
+
 const navigation = [
   { label: 'Home', path: '/' },
   { label: 'Profile', path: '/profile' },
 ]
-
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(' ')
-}
 
 const NevMenu: React.FC<any> = (props) => {
   const location = useLocation()
 
   return (
     <>
-      <div>
-        <Disclosure as="nav" className="bg-gray-800">
-          {({ open }) => (
-            <>
-              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-16">
-                  <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <img
-                        className="h-8 w-8"
-                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                        alt="Workflow"
-                      />
-                    </div>
-                    <div className="hidden md:block">
-                      <div className="ml-10 flex items-baseline space-x-4">
-                        {navigation.map((item, itemIdx) =>
-                          location.pathname === item.path ? (
-                            <React.Fragment key={itemIdx}>
-                              {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                              <Link
-                                to={item.path}
-                                className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
-                              >
-                                {item.label}
-                              </Link>
-                            </React.Fragment>
-                          ) : (
+      <Disclosure as="nav" className="sticky top-0 bg-yellow-900">
+        {({ open }) => (
+          <>
+            <div className="max-w-7xl mx-2 px-4 sm:px-6 lg:px-8">
+              <div className="flex items-center justify-between h-16">
+                <div className="flex items-center">
+                  <div className="flex-shrink-0">
+                    <img
+                      className="h-8 w-8 animate-bounce ease duration-100"
+                      src={myIcon}
+                      alt="Workflow"
+                    />
+                  </div>
+                  <div className="hidden md:block">
+                    <div className="ml-10 flex items-baseline space-x-4">
+                      {navigation.map((item, itemIdx) =>
+                        location.pathname === item.path ? (
+                          <React.Fragment key={itemIdx}>
+                            {/* Current: "bg-yellow-500 text-white", Default: "text-gray-300 hover:bg-yellow-700 hover:text-white" */}
                             <Link
                               to={item.path}
-                              className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                              className="bg-yellow-500 text-white px-3 py-2 rounded-md text-sm font-medium"
                             >
                               {item.label}
                             </Link>
-                          ),
-                        )}
-                      </div>
+                          </React.Fragment>
+                        ) : (
+                          <Link
+                            to={item.path}
+                            className="text-gray-300 hover:bg-yellow-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                          >
+                            {item.label}
+                          </Link>
+                        ),
+                      )}
                     </div>
                   </div>
-                  <div className="hidden md:block">
-                    <div className="ml-4 flex items-center md:ml-6">
-                      <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                        <span className="sr-only">View notifications</span>
-                        {/* <BellIcon className="h-6 w-6" aria-hidden="true" /> */}
-                      </button>
+                </div>
+                <div className="hidden md:block">
+                  <div className="ml-4 flex items-center md:ml-6">
+                    <button className="bg-yellow-900 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                      <span className="sr-only">View notifications</span>
+                      {/* <BellIcon className="h-6 w-6" aria-hidden="true" /> */}
+                    </button>
 
-                      {/* Profile dropdown */}
-                      {/* <Menu as="div" className="ml-3 relative">
+                    {/* Profile dropdown */}
+                    {/* <Menu as="div" className="ml-3 relative">
                         {({ open }) => (
                           <>
                             <div>
@@ -110,104 +107,51 @@ const NevMenu: React.FC<any> = (props) => {
                           </>
                         )}
                       </Menu> */}
-                    </div>
-                  </div>
-                  <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        // <XIcon className="block h-6 w-6" aria-hidden="true" />
-                        <span>X</span>
-                      ) : (
-                        // <MenuIcon className="block h-6 w-6" aria-hidden="true" />
-                        <span>Icon</span>
-                      )}
-                    </Disclosure.Button>
                   </div>
                 </div>
-              </div>
-
-              <Disclosure.Panel className="md:hidden">
-                <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                  {navigation.map((item, itemIdx) =>
-                    location.pathname === item.path ? (
-                      <React.Fragment key={itemIdx}>
-                        {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                        <a
-                          href="#"
-                          className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-                        >
-                          {item.label}
-                        </a>
-                      </React.Fragment>
+                <div className="-mr-2 flex md:hidden">
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      // <XIcon className="block h-6 w-6" aria-hidden="true" />
+                      <span>X</span>
                     ) : (
-                      <a
-                        key={itemIdx}
-                        href="#"
-                        className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                      >
-                        {item.label}
-                      </a>
-                    ),
-                  )}
+                      // <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                      <span>Icon</span>
+                    )}
+                  </Disclosure.Button>
                 </div>
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-      </div>
-      {/* <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-        <nav
-          className="relative flex items-center justify-between sm:h-10 lg:justify-start"
-          aria-label="Global"
-        >
-          <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-            <div className="flex items-center justify-between w-full md:w-auto">
-              <Link to="/">
-                <span className="sr-only">Workflow</span>
-                <img
-                  alt="first SY"
-                  className="h-8 w-auto sm:h-10"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                />
-              </Link>
-              <div className="-mr-2 flex items-center md:hidden">
-                <button
-                  type="button"
-                  className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                  aria-expanded="false"
-                >
-                  <span className="sr-only">Open main menu</span>
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                </button>
               </div>
             </div>
-          </div>
-          <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-            <Link to={'/'} className="font-medium text-gray-500 hover:text-gray-900">
-              HomePage
-            </Link>
-            <Link to={'/profile'} className="font-medium text-gray-500 hover:text-gray-900">
-              Profile
-            </Link>
-          </div>
-        </nav>
-      </div> */}
+
+            <Disclosure.Panel className="md:hidden">
+              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                {navigation.map((item, itemIdx) =>
+                  location.pathname === item.path ? (
+                    <React.Fragment key={itemIdx}>
+                      {/* Current: "bg-yellow-500 text-white", Default: "text-gray-300 hover:bg-yellow-700 hover:text-white" */}
+                      <Link
+                        to={item.path}
+                        className="bg-yellow-500 text-white block px-3 py-2 rounded-md text-base font-medium"
+                      >
+                        {item.label}
+                      </Link>
+                    </React.Fragment>
+                  ) : (
+                    <Link
+                      to={item.path}
+                      className="text-gray-300 hover:bg-yellow-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                    >
+                      {item.label}
+                    </Link>
+                  ),
+                )}
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
     </>
   )
 }
